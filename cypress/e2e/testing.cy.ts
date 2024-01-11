f2001423af6d59e23735f69d685e67333bec0438
brenohq <brenosc2@hotmail.com>
2024-01-11 19:04:55 -0300

    using data-test attribute instead of formcontrolname

--- cypress/e2e/testing.cy.ts
+++ cypress/e2e/testing.cy.ts
@@ -3,10 +3,10 @@ describe('Sign up flow', () => {
     cy.visit('http://localhost:4200');
     cy.contains('a', 'Register now').click();
 
-    cy.get('[formcontrolname="email"]').type('brenosc2@email.com');
-    cy.get('[formcontrolname="fullName"]').type('Breno Henrique');
-    cy.get('[formcontrolname="userName"]').type('brenosc2');
-    cy.get('[formcontrolname="password"]').type('pswdteste123');
+    cy.get('[data-test="email"]').type('brenosc2@email.com');
+    cy.get('[data-test="fullName"]').type('Breno Henrique');
+    cy.get('[data-test="registerUserName"]').type('brenosc2');
+    cy.get('[data-test="registerPassword"]').type('pswdteste123');
 
     cy.contains('button', 'Register').click();
   });
