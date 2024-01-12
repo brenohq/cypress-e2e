22574b3b3b17b47c9dee73046020055809a59b20
brenohq <brenosc2@hotmail.com>
2024-01-12 18:47:27 -0300

    defining custom command 'login'

--- cypress/e2e/correct-login.cy.ts
+++ cypress/e2e/correct-login.cy.ts
@@ -1,11 +1,9 @@
 describe('Sign in flow', () => {
+
   it('Fill the form to login a valid user', () => {
     cy.visit('http://localhost:4200');
 
-    cy.get('[data-test="loginUserName"]').type('brenosc2');
-    cy.get('[data-test="loginPassword"]').type('pswdteste123');
-
-    cy.get('[data-test="loginBtn"]').click();
+    cy.login('brenosc2', 'pswdteste123');
 
     cy.url().should('eq', 'http://localhost:4200/#/user/brenosc2');
   });
