916effc8fb24538748cfd91d55702b54f335963f
brenohq <brenosc2@hotmail.com>
2024-01-12 18:47:12 -0300

    cypress dashboard project linking

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "dv4cug",
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
