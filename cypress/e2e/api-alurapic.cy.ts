b7746fea49e3ad0dba687d1317ad7d93f08bbfc2
brenohq <brenosc2@hotmail.com>
2024-01-15 15:50:16 -0300

    another API call test case

--- cypress/e2e/api-alurapic.cy.ts
+++ cypress/e2e/api-alurapic.cy.ts
@@ -1,5 +1,5 @@
 describe('API test suite', () => {
-  it('API', () => {
+  it('User login endpoint', () => {
     cy.request({
       method: 'POST',
       url: 'http://localhost:3000/user/login',
@@ -9,6 +9,18 @@ describe('API test suite', () => {
       expect(res.body).is.not.empty;
       expect(res.body).to.have.property('id');
       expect(res.body.id).to.be.equal(3);
-    })
-  })
+    });
+  });
+
+  it('User photos endpoint', () => {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:3000/brenosc2/photos'
+    }).then((res) => {
+      expect(res.status).to.be.equal(200);
+      expect(res.body).is.not.empty;
+      expect(res.body[0]).to.have.property('description');
+      expect(res.body[0].description).to.be.equal('porquinho');
+    });
+  });
 });
