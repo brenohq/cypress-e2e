92c8f50ed58991d0c4b30fb2da2e44d0fa64dc6b
brenohq <brenosc2@hotmail.com>
2024-01-15 10:29:33 -0300

    using stup approach to intercept API calls

--- cypress/e2e/incorrect-login.cy.ts
+++ cypress/e2e/incorrect-login.cy.ts
@@ -1,8 +1,18 @@
 describe('Sign in flow', () => {
-  it('Empty state should have error messages', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:4200');
+    cy.intercept('POST', 'http://localhost:3000/user/login', {
+      statusCode: 401,
+    }).as('stubPost');
+  });
 
+  it('Empty state should have error messages', () => {
     cy.contains('User name is required!').should('be.visible');
     cy.contains('Password is required!').should('be.visible');
+  });
+
+  it('Fail when incorrect user input', () => {
+    cy.login('aaaaaa', 'aaaaa');
+    cy.wait('@stubPost');
   });
 })
